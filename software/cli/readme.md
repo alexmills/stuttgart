09ab12cf17e47b6b073ac890aefca4d39f85292b
Alex Mills <alex@acm.io>
2026-09-04 09:03:47 +0800

    Base CLI Project

---
+++ software/cli/readme.md
@@ -0,0 +1,10 @@
+# Stuttgart Command Line Client
+
+## Build
+
+```
+cmake -B build
+cmake --build build
+
+// ./build/stuttgart
+```
---
+++ software/cli/src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define STG_VERSION "0.1.0"
+
+int main(void) {
+    printf("Stuggart %s\n", STG_VERSION);
+    return 0;
+}
+
